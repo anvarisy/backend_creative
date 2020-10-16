9b142ad2fd6dce82827ff16705109e39cd3e675e
Andri nova riswanto <anvarisy@gmail.com>
2020-10-16 17:21:12 +0700

    Insya Allah Bisa

--- api/serializer.py
+++ api/serializer.py
@@ -7,9 +7,10 @@ class TypeSerializer(serializers.ModelSerializer):
         fields = ('id','style_id','type_name','type_icon')
 
 class StyleSerializer(serializers.ModelSerializer):
+    types = TypeSerializer(many=True)
     class Meta:
         model = style
-        fields = ('id','category_id','style_name','style_price','style_icon')
+        fields = ('id','category_id','style_name','style_price','style_icon','types')
 
 class CategorySerializer(serializers.ModelSerializer):
     styles = StyleSerializer(many=True)
