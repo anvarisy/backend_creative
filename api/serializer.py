d8c60818bcf68adda86198a82f0da813c2d66ff3
Andri nova riswanto <anvarisy@gmail.com>
2020-10-16 16:12:15 +0700

    Bismillah

--- api/serializer.py
+++ api/serializer.py
@@ -26,5 +26,5 @@ class OrderSeriliazer(serializers.ModelSerializer):
     key_user = serializers.CharField(source='user.c_user')
     class Meta:
         model = order
-        fields = ('order_id','user_id','key_user','date_request','style_id','order_image','order_result','is_remove_acc','is_include_file','is_fast','is_payed','is_finish')
+        fields = ('order_id','user_id','key_user','order_type','date_request','style_id','order_image','order_result','is_remove_acc','is_include_file','is_fast','is_payed','is_finish')
     
