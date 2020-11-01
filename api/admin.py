c71c253640d2834ae754d1d43ad2cfe89b5355c0
Andri nova riswanto <anvarisy@gmail.com>
2020-11-01 08:23:09 +0700

    Added admin

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from api.models import carousel, category, gallery, order, style, types, user
+from api.models import carousel, category, gallery, order, style, types, user,\
+    imageorder
 
 # Register your models here.
-admin.site.register([category,style,user,order, types, carousel, gallery])
+admin.site.register([category,style,user,order, types, carousel, gallery,imageorder])
